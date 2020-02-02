f9f6eea23a1781649b2d5a8144198da950892330
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2020-02-02 22:03:58 +0100

    return error

--- process.go
+++ process.go
@@ -7,35 +7,35 @@ import (
 	"strings"
 )
 
-func processMessage(m message) {
+func processMessage(m message) error {
 	fmt.Printf("Size: %v, Date: %v, Snippet: %q\n", m.size, m.date, m.snippet)
 	log.Println("Extract validation url")
 	url, err := extractValidationUrl(strings.NewReader(m.body))
 	if err != nil {
-		log.Fatalf("Url not found in message %v: %v", m.gmailID, m)
+		return fmt.Errorf("Url not found in message %v: %v", m.gmailID, m)
 	}
 
 	log.Println("Follow redirect url")
 	content, err := followUrl(url)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	log.Println("Redirect url OK")
 
 	log.Println("Extract redirect url")
 	redirectUrl, err := extractRedirectUrl(strings.NewReader(string(content)))
 	if err != nil {
-		log.Fatalf("Redirect url not found in response from validation link: %s", string(content))
+		return fmt.Errorf("Redirect url not found in response from validation link: %s", string(content))
 	}
 
 	content2, err := followUrl(redirectUrl)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	players, err := parseAttendeePage(strings.NewReader(string(content2)))
 	if err != nil {
-		log.Fatalf("error parsing attendee page: %v", err)
+		return fmt.Errorf("error parsing attendee page: %v", err)
 	}
 
 	if len(players) > 0 {
@@ -47,4 +47,5 @@ func processMessage(m message) {
 			}
 		}
 	}
+	return nil
 }
